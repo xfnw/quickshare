c363291982ed6292f40e88da2f110664bd8164ed
xfnw <xfnw@riseup.net>
2023-11-19 13:43:49 -0500

    parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "quickshare", about = "quickly spin up a file upload form")]
+struct Opt {
+    #[structopt(short, env = "PORT", default_value = "3000")]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
 }

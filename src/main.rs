0a9c3542659e400aa81be3c4f5cada19d7b66aa7
xfnw <xfnw@riseup.net>
2025-09-05 15:33:21 -0400

    add patchbay-style piping
    rip patchbay.pub

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 #![deny(clippy::pedantic)]
 
 use argh::FromArgs;
-use std::{fs::File, include_str, io::prelude::*, net::SocketAddr, sync::Arc};
-use tokio::net::TcpListener;
+use std::{
+    collections::BTreeMap, fs::File, include_str, io::prelude::*, net::SocketAddr, sync::Arc,
+};
+use tokio::{
+    net::TcpListener,
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Mutex, MutexGuard,
+    },
+};
 use tower_http::services::ServeDir;
 
 use axum::{
-    extract::{DefaultBodyLimit, Multipart, State},
+    body::Body,
+    extract::{DefaultBodyLimit, Multipart, Path, State},
     http::{header::HeaderMap, StatusCode},
     response::Html,
     routing::{get, post},
@@ -16,6 +25,7 @@ use axum::{
 /// quickly spin up a file upload form
 #[derive(Debug, FromArgs)]
 #[argh(help_triggers("-h", "--help"))]
+#[allow(clippy::struct_excessive_bools)]
 struct Opt {
     /// socket address to bind
     #[argh(option, short = 'b', default = "\"[::]:3000\".parse().unwrap()")]
@@ -32,10 +42,29 @@ struct Opt {
     /// turn off uploading files
     #[argh(switch)]
     no_upload: bool,
+    /// turn off piping
+    #[argh(switch)]
+    no_pipe: bool,
 }
 
 struct AppState {
     unprefixed: bool,
+    pipes: Mutex<BTreeMap<String, Pipe>>,
+}
+
+struct Pipe {
+    sender: Arc<Mutex<Sender<Body>>>,
+    receiver: Arc<Mutex<Receiver<Body>>>,
+}
+
+impl Pipe {
+    fn new() -> Self {
+        let (sender, receiver) = channel(1);
+        Self {
+            sender: Arc::new(Mutex::new(sender)),
+            receiver: Arc::new(Mutex::new(receiver)),
+        }
+    }
 }
 
 async fn root() -> Html<&'static str> {
@@ -92,11 +121,59 @@ async fn upload(
     Err((StatusCode::BAD_REQUEST, "no file? 😳".to_string()))
 }
 
+fn pipecleaner(pipes: &mut MutexGuard<BTreeMap<String, Pipe>>) {
+    // kinda wasteful to do this every time, but manually
+    // figuring out when to remove stuff is complicated
+    pipes.retain(|_, pipe| {
+        Arc::strong_count(&pipe.sender) > 1 || Arc::strong_count(&pipe.receiver) > 1
+    });
+}
+
+async fn recv_pipe(
+    Path(name): Path<String>,
+    State(state): State<Arc<AppState>>,
+) -> Result<Body, (StatusCode, &'static str)> {
+    let receiver = {
+        let mut pipes = state.pipes.lock().await;
+        pipecleaner(&mut pipes);
+        pipes.entry(name).or_insert_with(Pipe::new).receiver.clone()
+    };
+    let mut receiver = receiver.lock().await;
+
+    receiver
+        .recv()
+        .await
+        .ok_or((StatusCode::INTERNAL_SERVER_ERROR, "the channel closed???\n"))
+}
+
+async fn send_pipe(
+    Path(name): Path<String>,
+    State(state): State<Arc<AppState>>,
+    body: Body,
+) -> Result<(), (StatusCode, String)> {
+    let sender = {
+        let mut pipes = state.pipes.lock().await;
+        pipecleaner(&mut pipes);
+        pipes.entry(name).or_insert_with(Pipe::new).sender.clone()
+    };
+    let sender = sender.lock().await;
+
+    unwrap_or_bad!(sender.send(body).await);
+    // cursed way to pretend mpsc waits for the receiver,
+    // instead of returning immediately when there is capacity,
+    // since tokio does not allow having 0 capacity :(
+    // this only works because sender is locked behind a Mutex
+    unwrap_or_bad!(sender.reserve().await);
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
     let opt: Opt = argh::from_env();
     let state = Arc::new(AppState {
         unprefixed: opt.unprefixed,
+        pipes: Mutex::new(BTreeMap::new()),
     });
     let app = Router::new();
 
@@ -104,6 +181,13 @@ async fn main() {
         app
     } else {
         app.route("/", get(root)).route("/", post(upload))
+    };
+
+    let app = if opt.no_pipe {
+        app
+    } else {
+        app.route("/pipe/{name}", get(recv_pipe))
+            .route("/pipe/{name}", post(send_pipe))
     };
 
     let app = app
